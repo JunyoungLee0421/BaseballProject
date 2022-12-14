65456ce8e9dc94386a94d665fe224c68536a9d51
EunjeongHur <ungdunge2@gmail.com>
2022-12-13 21:41:59 -0800

    add checking invalid input feature

--- script/practice.js
+++ script/practice.js
@@ -1,4 +1,5 @@
 let counter = 0;
+let generated_number;
 
 setRandomNumber = function(digit) {
 
@@ -14,14 +15,15 @@ setRandomNumber = function(digit) {
     return generated_number
 }
 
-inputnumberBtnHandler = function(generated_num, guess_num) {
+inputnumberBtnHandler = function(guess_num) {
+    console.log(generated_number)
     var strike = 0;
     var ball = 0;
     var hint = ""
     var guess_list = Array.from(guess_num)
-    var secret_numlist = Array.from(generated_num)
+    var secret_numlist = Array.from(generated_number)
 
-    for (var i = 0; i < generated_num.length; i++){
+    for (var i = 0; i < generated_number.length; i++){
         if (guess_list[i] == secret_numlist[i]){
             strike += 1
         } else if (secret_numlist.includes(guess_list[i])){
@@ -47,19 +49,34 @@ inputnumberBtnHandler = function(generated_num, guess_num) {
     counter += 1
 }
 
+
+checklength = function(guess_num) {
+    return guess_num.length == selected_type
+}
+
+checkrepeat = function(guess_num){
+    return !/(\w).*\1/i.test(guess_num)
+}
+
+
+$("#digitType_box").change(function() {
+    counter = 0
+    selected_type = $("input[name='digitType']:checked").val();
+    generated_number = setRandomNumber(Number(selected_type))
+    $("#result").empty()
+})
+
+
 setup = function(){
+    selected_type = $("input[name='digitType']:checked").val();
     generated_number = setRandomNumber(3)
     $("#inputnumberBtn").click(function() {
         guess_num = $("#inputnumber").val()
-        inputnumberBtnHandler(generated_number, guess_num)
-    })
-
-
-    $("#digitType_box").change(function() {
-        counter = 0
-        selected_type = $("input[name='digitType']:checked").val();
-        generated_number = setRandomNumber(Number(selected_type))
-        $("#result").empty()
+        if (checklength(guess_num) && checkrepeat(guess_num)){
+            inputnumberBtnHandler(guess_num)
+        } else {
+            console.log("Invalid input")
+        }
     })
 }
 
