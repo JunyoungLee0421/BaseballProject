c6da1be8e38432aea0c6421b97e0e1a1820ca535
EunjeongHur <ungdunge2@gmail.com>
2023-01-01 13:46:14 -0800

    check number of realtime users

--- script/pvp_matching.js
+++ script/pvp_matching.js
@@ -6,8 +6,23 @@ quitRoom = function(){
     })
 }
 
+checkusers = function(){
+    let params = new URL(window.location.href);
+    let docid = params.searchParams.get("id");
+    db.collection("rooms").doc(docid)
+        .onSnapshot((doc) => {
+            let users = doc.data().users;
+            console.log(users.length)
+            if (users.length == 2){
+                $("#loading").css('display', 'none')
+                // do something
+            }
+        })
+}
+
 setup = function(){
     $("#quit_room").click(quitRoom)
+    checkusers()
 }
 
 $(document).ready(setup);
